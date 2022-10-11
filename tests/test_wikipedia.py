109fed31fc1bd6c2d516ee7659d1152fecbdf9c5
A-Prudnikova <a.prudnikova@gmail.com>
2022-10-11 16:13:28 +0300

    browserstack 401

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -4,29 +4,13 @@ from appium.webdriver.common.appiumby import AppiumBy
 from selene import have
 from selene.support.shared import browser
 
-from util.attachment import add_video
-
 
 @allure.tag('mobile')
 @allure.title('Test search')
-def test_wiki_browserstack(app_android):
+def test_wiki_browserstack(create_driver):
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('BrowserStack')
     with step('Verify content found'):
         browser.all((AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title'))\
             .should(have.size_greater_than(0))
-    add_video(browser)
-
-
-@allure.tag('mobile')
-@allure.title('Test search')
-def test_wiki_sqa(app_android):
-    with step('Type search'):
-        browser.element((AppiumBy.ACCESSIBILITY_ID, 'Search Wikipedia')).click()
-        browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type("Software quality assurance")
-    with step('Verify content found'):
-        browser.all(
-            (AppiumBy.ID, 'org.wikipedia.alpha:id/page_list_item_title')
-        ).should(have.size_greater_than(0))
-    add_video(browser)
